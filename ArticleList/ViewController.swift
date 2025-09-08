6f8df129ee61a7a896bada9ffb53703744732369
Dhathri33 <dbathini@luc.edu>
2025-09-08 15:12:42 -0500

    Initial Commit

---
+++ ArticleList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleList
+//
+//  Created by Dhathri Bathini on 9/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
