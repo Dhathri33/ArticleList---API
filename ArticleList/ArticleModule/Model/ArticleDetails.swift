08538763235770e63d7451edbadd1b3b23b6c35d
sathvika-muthyala <sathvika141.ms@gmail.com>
2025-09-29 13:05:07 -0400

    checking rebase

--- ArticleList/ArticleModule/Model/ArticleDetails.swift
+++ ArticleList/ArticleModule/Model/ArticleDetails.swift
@@ -27,7 +27,7 @@ struct ArticleDetails: Decodable {
     var author: String?
     let description: String?
     let urlToImage: String?
-    
+    // Property Wrapper
     @DateOnly var publishedAt: String?
     
     var comments: String? {
